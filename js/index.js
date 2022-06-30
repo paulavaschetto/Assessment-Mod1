05362d5d35b8d9a1022dc3b24ebc3ef11d70c887
paula <106301749+paulavaschetto@users.noreply.github.com>
2022-06-30 22:30:30 +1200

    remove from cart feature

--- js/index.js
+++ js/index.js
@@ -8,48 +8,47 @@ function openMenu() {
     }
   }
 
-//   function increaseCount(a, b) {
-//     var input = b.previousElementSibling;
-//     var value = parseInt(input.value, 10);
-//     value = isNaN(value) ? 0 : value;
-//     value++;
-//     input.value = value;
-//   }
-  
-//   function decreaseCount(a, b) {
-//     var input = b.nextElementSibling;
-//     var value = parseInt(input.value, 10);
-//     if (value > 0) {
-//       value = isNaN(value) ? 0 : value;
-//       value--;
-//       input.value = value;
-//     }
-//   }
 
     //initialising a variable name data
     var data = 0;
+   
   
-    //printing default value of data that is 0 in h2 tag
+   //printing default value of data that is 0 in h2 tag
     document.getElementById("counting").innerText = data;
-    document.getElementById("counting-check").innerText = data;
 
-    //creation of increment function
+
+   //*creation of increment function*//
     function increment() {
         data = data + 1;
         document.getElementById("counting").innerText = data;
     }
-    //creation of decrement function
+   //creation of decrement function//
     function decrement() {
         data = data - 1;
         document.getElementById("counting").innerText = data;
     }
 
-    function increment() {
-        data = data + 1;
-        document.getElementById("counting-check").innerText = data;
+    var dataCheckout = 1;
+    document.getElementById("counting-check").innerText = dataCheckout;
+
+    function incrementCheck() {
+        dataCheckout = dataCheckout + 1;
+        document.getElementById("counting-check").innerText = dataCheckout;
     }
-    //creation of decrement function
-    function decrement() {
-        data = data - 1;
-        document.getElementById("counting-check").innerText = data;
+    //creation of decrement function//
+    function decrementCheck() {
+      dataCheckout = dataCheckout - 1;
+        document.getElementById("counting-check").innerText = dataCheckout;
+    }
+
+    // remove item from cart
+
+    var removeCartItem = document.getElementsByClassName("delete-btn");
+    console.log(removeCartItem);
+    for (var i = 0; i < removeCartItem.length; i++) {
+      var button = removeCartItem[i]
+      button.addEventListener('click', function(event){
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+      })
     }
