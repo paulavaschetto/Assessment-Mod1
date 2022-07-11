3176559d4ee32a7668d8c586ad96c0be5bc3103d
paula <106301749+paulavaschetto@users.noreply.github.com>
2022-07-11 20:30:36 +1200

    style tidy-up

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function openMenu() {
     var x = document.getElementById("menuLinks");
     if (x.style.display === "block") {
@@ -9,46 +8,17 @@ function openMenu() {
   }
 
 
-    //initialising a variable name data
+    
     var data = 0;
    
-  
-   //printing default value of data that is 0 in h2 tag
     document.getElementById("counting").innerText = data;
 
-
-   //*creation of increment function*//
     function increment() {
         data = data + 1;
         document.getElementById("counting").innerText = data;
     }
-   //creation of decrement function//
+ 
     function decrement() {
         data = data - 1;
         document.getElementById("counting").innerText = data;
-    }
-
-    // var dataCheckout = 1;
-    // document.getElementById("counting-check").innerText = dataCheckout;
-
-    // function incrementCheck() {
-    //     dataCheckout = dataCheckout + 1;
-    //     document.getElementById("counting-check").innerText = dataCheckout;
-    // }
-    // //creation of decrement function//
-    // function decrementCheck() {
-    //   dataCheckout = dataCheckout - 1;
-    //     document.getElementById("counting-check").innerText = dataCheckout;
-    // }
-
-    // remove item from cart
-
-    var removeCartItem = document.getElementsByClassName("delete-btn");
-    console.log(removeCartItem);
-    for (var i = 0; i < removeCartItem.length; i++) {
-      var button = removeCartItem[i]
-      button.addEventListener('click', function(event){
-        var buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.remove()
-      })
     }
